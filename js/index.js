cdd5f47bbf7555dcc308a18db81220d5d4fe7654
KooriMoe <dev@zihan.moe>
2022-04-07 22:14:01 +0800

    🆕 Add 济南-车站街邮政支局

--- js/index.js
+++ js/index.js
@@ -28,3 +28,9 @@ lightGallery(document.getElementById('bjznh'), {
     licenseKey: '0000-0000-000-0000',
     width: '300px'
 });
+lightGallery(document.getElementById('jnczj'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    licenseKey: '0000-0000-000-0000',
+    width: '300px'
+});
