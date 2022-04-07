263f540150c04bdb27dacf42e61d4a94da3e02ed
KooriMoe <dev@zihan.moe>
2022-04-07 22:07:38 +0800

    ⚠️ Bug fix.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
-lightGallery(document.getElementById('lightgallery'), {
+lightGallery(document.getElementById('shrmgy'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    licenseKey: '0000-0000-000-0000',
+    width: '300px'
+});
+lightGallery(document.getElementById('hzzh'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    licenseKey: '0000-0000-000-0000',
+    width: '300px'
+});
+lightGallery(document.getElementById('bj2022'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    licenseKey: '0000-0000-000-0000',
+    width: '300px'
+});
+lightGallery(document.getElementById('tsinghua'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    licenseKey: '0000-0000-000-0000',
+    width: '300px'
+});
+lightGallery(document.getElementById('bjznh'), {
     plugins: [lgZoom, lgThumbnail],
     speed: 500,
     licenseKey: '0000-0000-000-0000',
